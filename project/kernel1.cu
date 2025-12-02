4c320d1c39012a58db8a71c315ee73f85fc98662
Mikhail Kornilov <mkornilov07@gmail.com>
2025-12-02 14:48:28 -0500

    start binary search through B strategy in kernel1.cu

--- project/kernel1.cu
+++ project/kernel1.cu
@@ -1,9 +1,10 @@
-
+// Mikhail
+// Binary search through B
 #include "common.h"
 #include "timer.h"
 
 void spmspm_gpu1(CSRMatrix* csrMatrix1, CSRMatrix* csrMatrix2, CSRMatrix* csrMatrix1_d, CSRMatrix* csrMatrix2_d, COOMatrix* cooMatrix_d) {
-
+    
 
 
 
