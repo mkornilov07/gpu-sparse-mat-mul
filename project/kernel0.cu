9aad6fa7b887af24b2bd9da40730aa7c0fa07fdc
Daniel Lee <vjw9xf@virginia.edu>
2025-12-08 12:35:48 -0500

    Implemented linear colIdx search base kernel0

--- project/kernel0.cu
+++ project/kernel0.cu
@@ -2,70 +2,116 @@
 #include "common.h"
 #include "timer.h"
 
+// Does not work, depends on ordered colIdxs.
+/*
+__global__ void spmspm_kernel0(CSRMatrix* A, CSRMatrix* B, COOMatrix* C) {
+        unsigned int rowA = blockIdx.x * blockDim.x + threadIdx.x;
+        if (rowA < A->numRows) {
+                unsigned int beginRowA = A->rowPtrs[rowA];
+                unsigned int endRowA = A->rowPtrs[rowA + 1];
+                unsigned int bufferSize = endRowA - beginRowA;
 
-#include "common.h"
-#include "timer.h"
-__global__
-void spmspm_kernel0(const CSRMatrix* A, const CSRMatrix* B, 
-                    COOMatrix* C, float* array) {
+                float valBufferA[bufferSize];
+                unsigned int rowPtrBufferB[bufferSize];
+                unsigned int rowPtrMaxesB[bufferSize];
+                
+                for (int i = beginRowA; i < endRowA; ++i) {
+                        valBufferA[i - beginRowA] = A->values[i];
+                        
+                        rowPtrBufferB[i - beginRowA] = B->rowPtrs[A->colIdxs[i]];
+                        rowPtrMaxesB[i - beginRowA] = B->rowPtrs[A->colIdxs[i] + 1];
+                }
 
-    // find which row to work in
-    int row = blockIdx.x * blockDim.x + threadIdx.x;
-
-    // check boundary condition
-    if (row >= A->numRows) return;
-
-    // array to accumulate vals for each col value in row
-    float* valArray = &array[row * B->numCols];
-
-    // initializing array
-    for (int j = 0; j < B->numCols; j++)
-        valArray[j] = 0.0f;
-
-    // get start and end indices for cols
-    int aStart = A->rowPtrs[row];
-    int aEnd = A->rowPtrs[row + 1];
-
-    for (int i = aStart; i < aEnd; i++) {
-        int col_a = A->colIdxs[i];
-        float aVal = A->values[i];
-
-        // use col_a as rows of b
-        int bStart = B->rowPtrs[col_a];
-        int bEnd   = B->rowPtrs[col_a+1];
-
-        for (int k = bStart; k < bEnd; k++) {
-            int col_b = B->colIdxs[k];
-            float bVal = B->values[k];
-                    
-            // accumulate dot product of A(i, j) * B(J, k) into valArray[k]
-            valArray[col_b] += aVal * bVal;
+                while(true) {
+                        float dotProduct = 0.0f;
+                        unsigned int minCol = 0xFFFFFFFF;
+                        for (int i = 0; i < bufferSize; ++i) {
+                                if (rowPtrBufferB[i] < rowPtrMaxesB[i]) {
+                                        unsigned int colIdxB = B->colIdxs[rowPtrBufferB[i]];
+                                        if (colIdxB < minCol) {
+                                                minCol = colIdxB;
+                                                dotProduct = valBufferA[i] * B->values[rowPtrBufferB[i]];
+                                        }
+                                        else if (colIdxB == minCol) {
+                                                dotProduct += valBufferA[i] * B->values[rowPtrBufferB[i]];
+                                        }
+                                }
+                        }
+                        for (int i = 0; i < bufferSize; ++i) {
+                                if (rowPtrBufferB[i] < rowPtrMaxesB[i] && B->colIdxs[rowPtrBufferB[i]] == minCol) ++rowPtrBufferB[i];
+                        }
+                        if (dotProduct == 0.0f) break; // Assumes all matrix values are positive, same as is assumed in the CPU implementation
+                        else {
+                                unsigned int pos = atomicAdd(&C->numNonzeros, 1u);
+                                C->rowIdxs[pos] = rowA;
+                                C->colIdxs[pos] = minCol;
+                                C->values[pos] = dotProduct;
+                        }
+                }
         }
-    }
+}
+*/
 
-     // write non zero valuees to COO matrix C
-    for (int j = 0; j < B->numCols; j++) {
-        float v = valArray[j];
-        if (v != 0.0f) {
-            int pos = atomicAdd(&C->numNonzeros, 1);
-            C->rowIdxs[pos] = row;
-            C->colIdxs[pos] = j;
-            C->values[pos] = v;
+// Same as kernel below except one thread per row instead of output element.
+// Much slower than kernel below.
+__global__ void spmspm_kernel0_noarrays(CSRMatrix* A, CSRMatrix* B, COOMatrix* C) {
+        // AB = C, where C_ij = sum_k{ A_ik B_kj } 
+        unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+        if (i >= A->numRows) return;
+        unsigned int kBeginPtr = A->rowPtrs[i];
+        unsigned int kEndPtr = A->rowPtrs[i + 1];
+        for (unsigned int j = 0; j < B->numCols; ++j) {
+                float dotProduct = 0.0f;
+                for (unsigned int kPtr = kBeginPtr; kPtr < kEndPtr; ++kPtr) {
+                        unsigned int k = A->colIdxs[kPtr];
+                        for (unsigned int jPtr = B->rowPtrs[k]; jPtr < B->rowPtrs[k + 1]; ++jPtr) {
+                                unsigned int jNonzero = B->colIdxs[jPtr];
+                                if (jNonzero == j) {
+                                        dotProduct += A->values[kPtr] * B->values[jPtr];
+                                        break;
+                                }
+                        }
+                }
+                if (dotProduct > 0.0f) { // Assumes all matrix values are positive, same as is assumed in the CPU implementation
+                        unsigned int pos = atomicAdd(&C->numNonzeros, 1u);
+                        C->rowIdxs[pos] = i;
+                        C->colIdxs[pos] = j;
+                        C->values[pos] = dotProduct;
+                }
         }
-    }
+}
+
+// One thread per output element with linear search for colIdx.
+__global__ void spmspm_kernel0_multidim(CSRMatrix* A, CSRMatrix* B, COOMatrix* C) {
+        // AB = C, where C_ij = sum_k{ A_ik B_kj }
+        unsigned int j = blockIdx.x * blockDim.x + threadIdx.x;
+        unsigned int i = blockIdx.y * blockDim.y + threadIdx.y;
+        if (i >= C->numRows || j >= C->numCols) return;
+        float dotProduct = 0.0f;
+        for (unsigned int kPtr = A->rowPtrs[i]; kPtr < A->rowPtrs[i + 1]; ++kPtr) {
+                unsigned int k = A->colIdxs[kPtr];
+                for (unsigned int jPtr = B->rowPtrs[k]; jPtr < B->rowPtrs[k + 1]; ++jPtr) {
+                        unsigned int jVal = B->colIdxs[jPtr];
+                        if (jVal == j) {
+                                dotProduct += A->values[kPtr] * B->values[jPtr];
+                                break;
+                        }
+                }
+        }
+        if (dotProduct > 0.0f) { // Assumes all matrix values are positive, same as is assumed in the CPU implementation
+                unsigned int pos = atomicAdd(&C->numNonzeros, 1u);
+                C->rowIdxs[pos] = i;
+                C->colIdxs[pos] = j;
+                C->values[pos] = dotProduct;
+        }
 }
 
 void spmspm_gpu0(CSRMatrix* csrMatrix1, CSRMatrix* csrMatrix2, CSRMatrix* csrMatrix1_d, CSRMatrix* csrMatrix2_d, COOMatrix* cooMatrix_d) {
-    
-    int numThreads = 128;
-    int numBlocks = (csrMatrix1->numRows + numThreads - 1) / numThreads;
-    float* array_d;
-    cudaMalloc(&array_d, sizeof(float) * csrMatrix1->numRows * csrMatrix2->numCols);
-    cudaMemset(array_d, 0, sizeof(float) * csrMatrix1->numRows * csrMatrix2->numCols);
-
-    spmspm_kernel0<<<numBlocks, numThreads>>>(csrMatrix1_d, csrMatrix2_d, cooMatrix_d, array_d);
-    cudaDeviceSynchronize();
-
+        printf("numrows: %d", csrMatrix1->numRows);
+        // After testing, 16 x 16 seems to be optimal   
+        dim3 threadsPerBlock(16, 16);
+        dim3 numBlocks((csrMatrix2->numCols + threadsPerBlock.x - 1) / threadsPerBlock.x, (csrMatrix1->numRows + threadsPerBlock.y - 1) / threadsPerBlock.y);
+        spmspm_kernel0_multidim<<<numBlocks, threadsPerBlock>>>(csrMatrix1_d, csrMatrix2_d, cooMatrix_d);
 }
 
 
