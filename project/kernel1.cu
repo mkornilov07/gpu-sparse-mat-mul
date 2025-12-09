ba5809b5b6eb64efe49c7fe1b57bdcc3010407df
Daniel Lee <vjw9xf@virginia.edu>
2025-12-09 01:36:28 -0500

    Implemented privatization kernel1 (+60ms slower than base)

--- project/kernel1.cu
+++ project/kernel1.cu
@@ -1,69 +1,70 @@
 #include "common.h"
 #include "timer.h"
-// Mikhail
-// One thread per element of C, Linear search through row in B
 
-#define DEBUG 0
+/*
+ * Parallel implementation of SpMSpM with privatization.
+ * One thread assigned to each row of A.
+ * 
+ * AB = C, where 
+ * A, B, C are N-by-N,
+ * A = B, and 
+ * C_ij = sum_k{ A_ik * B_kj }
+ */
+__global__ void spmspm_kernel1(
+                const CSRMatrix* A,
+                const CSRMatrix* B,
+                COOMatrix* C) {
+	extern __shared__ float fullyConstructedCRows[];
 
-__global__ void bin_search_B_kernel(CSRMatrix* A, CSRMatrix* B, COOMatrix* C) {
-    float sum = 0.; // sum A[i][k] * B[k][j]
-    int i = blockDim.x * blockIdx.x + threadIdx.x;
-    int j = blockDim.y * blockIdx.y + threadIdx.y;
-    if(i < A->numRows && j < B->numCols) {
-        for(int csrPtr = A->rowPtrs[i]; csrPtr < A->rowPtrs[i+1]; csrPtr += 1) {
-            int k = A->colIdxs[csrPtr];
-            float valA = A->values[csrPtr];
-            if(DEBUG) printf("(%d, %d, %d): value in A is %f\n", i, j, k, valA);
-            // need to find B[k, j] using binary search
-            for(int bPtr = B->rowPtrs[k]; bPtr < B->rowPtrs[k+1]; bPtr++) {
-                if(B->colIdxs[bPtr] == j) {
-                if(DEBUG) printf("(%d, %d, %d): found matching column %d in B, the value there is %f, our A value is %f\n", i, j, k, j, B->values[bPtr], valA);
-                sum += B->values[bPtr] * valA;
-            }
-            }
-            
-            }
-            
+	unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+        unsigned int N = A->numRows;
+        if (i >= N) return;
+
+	float* fullyConstructedCRow = &fullyConstructedCRows[threadIdx.x * N];
+	for (unsigned int initPtr = 0; initPtr < N; ++initPtr) fullyConstructedCRow[initPtr] = 0.0f;
+
+        for (unsigned int kPtr = A->rowPtrs[i]; kPtr < A->rowPtrs[i + 1]; ++kPtr) {
+                unsigned int k = A->colIdxs[kPtr];
+
+                for (unsigned int jPtr = B->rowPtrs[k]; jPtr < B->rowPtrs[k + 1]; ++jPtr) {
+                        unsigned int j = B->colIdxs[jPtr];
+
+                        // Accumulate dot products
+                        fullyConstructedCRow[j] += A->values[kPtr] * B->values[jPtr];
+                }
         }
-        if(sum != 0.) { 
-            //append (i, j, sum) to C atomically
-            if(DEBUG) printf("(%d, %d): writing sum %f\n", i, j, sum);
-            int insertIdx = atomicAdd(&(C->numNonzeros), 1);
-            C->rowIdxs[insertIdx] = i;
-            C->colIdxs[insertIdx] = j;
-            C->values[insertIdx] = sum;
-    }
-}
 
+        // Write nonzero dot products to COOMatrix C
+        // Assumes all matrix elements in A and B are 
+        // nonnegative (as is in the CPU implementation)
+	
+	//unsigned int numNonzeros = 0u;
+	//for (unsigned int j = 0; j < N; ++j) if (fullyConstructedCRow[j] != 0.0f) ++numNonzeros;
+	//unsigned int startingPos = atomicAdd(&C->numNonzeros, numNonzeros);
+	//unsigned int nonzero = 0u;
+        for (unsigned int j = 0; j < N; ++j) {
+                float dotProduct = fullyConstructedCRow[j];
+                if (dotProduct != 0.0f) {
+                        unsigned int pos = atomicAdd(&C->numNonzeros, 1u);
+                        C->rowIdxs[pos] = i;
+                        C->colIdxs[pos] = j;
+                        C->values[pos] = dotProduct;
+                	//++nonzero;
+		}
+        }
+}
 
 void spmspm_gpu1(CSRMatrix* csrMatrix1, CSRMatrix* csrMatrix2, CSRMatrix* csrMatrix1_d, CSRMatrix* csrMatrix2_d, COOMatrix* cooMatrix_d) {
-    dim3 threadsPerBlock(16, 16);
-    dim3 numBlocks(csrMatrix1->numRows / threadsPerBlock.x + 1, csrMatrix2->numCols / threadsPerBlock.y + 1);
-    bin_search_B_kernel <<< numBlocks, threadsPerBlock >>> (csrMatrix1_d, csrMatrix2_d, cooMatrix_d);
-
-
+	cudaDeviceProp prop;
+	cudaGetDeviceProperties(&prop, 0);
+	unsigned int N = csrMatrix1->numRows;
+	unsigned int threadsPerBlock = prop.sharedMemPerBlock / (N * sizeof(float));
+	if (threadsPerBlock > 0) {
+		unsigned int numBlocks = (N + threadsPerBlock - 1) / threadsPerBlock;
+		spmspm_kernel1<<<numBlocks, threadsPerBlock, threadsPerBlock * N * sizeof(float)>>>(csrMatrix1_d, csrMatrix2_d, cooMatrix_d);
+	}
+	else {
+		printf("(kernel1) Row(s) too large to fit in SM shared memory (N = %d, sharedMemPerBlock = %lu bytes. Defaulting to kernel0.\n", N, prop.sharedMemPerBlock);
+		spmspm_gpu0(csrMatrix1, csrMatrix2, csrMatrix1_d, csrMatrix2_d, cooMatrix_d);
+	}
 }
-
-
-
-
-// struct CSRMatrix {
-//     unsigned int numRows;
-//     unsigned int numCols;
-//     unsigned int numNonzeros;
-//     unsigned int* rowPtrs;
-//     unsigned int* colIdxs;
-//     float* values;
-// };
-
-
-
-// struct COOMatrix {
-//     unsigned int numRows;
-//     unsigned int numCols;
-//     unsigned int numNonzeros;
-//     unsigned int capacity;
-//     unsigned int* rowIdxs;
-//     unsigned int* colIdxs;
-//     float* values;
-// };
